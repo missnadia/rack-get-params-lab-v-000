2943434e9ba92800fb800e85c8ee98953754b2c3
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-02 20:11:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,13 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
+      resp.write handle_search(search_term)
     else
       resp.write "Path Not Found"
     end
